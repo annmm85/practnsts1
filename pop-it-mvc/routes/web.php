4dfd2a4511c78edfedc793e49e91760ac2ab096e
AnnMm85 <8mona854@gmail.com>
2024-04-12 13:12:45 +0700

    Реализация страницы выхода

--- pop-it-mvc/routes/web.php
+++ pop-it-mvc/routes/web.php
@@ -13,6 +13,7 @@ Route::add(['GET', 'POST'], '/create_students', [Controller\Site::class, 'create
 Route::add(['GET', 'POST'], '/create_groops', [Controller\Site::class, 'create_groops'])->middleware('auth','sotrudnik');
 Route::add(['GET', 'POST'], '/create_grades', [Controller\Site::class, 'create_grades'])->middleware('auth','sotrudnik');
 Route::add(['GET', 'POST'], '/login', [Controller\Site::class, 'login'])->middleware('noAuth');
+Route::add('GET', '/logout', [Controller\Site::class, 'logout'])->middleware('auth');
 
 
 
