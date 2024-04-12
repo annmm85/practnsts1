97455241fcd6d670e766044788bd5cead543494f
AnnMm85 <8mona854@gmail.com>
2024-04-12 13:00:17 +0700

    Реализация страницы создания сотрудников

--- pop-it-mvc/routes/web.php
+++ pop-it-mvc/routes/web.php
@@ -3,3 +3,4 @@
 use Src\Route;
 
 Route::add('GET', '/users', [Controller\Site::class, 'users'])->middleware('auth', 'admin');
+Route::add('GET', '/students', [Controller\Site::class, 'students'])->middleware('auth','sotrudnik');
