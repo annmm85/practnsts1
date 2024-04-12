bbd66b655e2e4ef5d1ed1adf52eaacb4c97630b0
AnnMm85 <8mona854@gmail.com>
2024-04-12 13:05:45 +0700

    Реализация страницы вывода групп

--- pop-it-mvc/routes/web.php
+++ pop-it-mvc/routes/web.php
@@ -5,3 +5,15 @@ use Src\Route;
 Route::add('GET', '/users', [Controller\Site::class, 'users'])->middleware('auth', 'admin');
 Route::add('GET', '/students', [Controller\Site::class, 'students'])->middleware('auth','sotrudnik');
 Route::add(['GET', 'POST'], '/create_disciplines', [Controller\Site::class, 'create_disciplines'])->middleware('auth','sotrudnik');
+Route::add('GET', '/groops', [Controller\Site::class, 'groops'])->middleware('auth','sotrudnik');
+
+
+
+
+
+
+
+
+
+
+
