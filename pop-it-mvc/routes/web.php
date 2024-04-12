c58f7f50e95bbed12eecbdb71aacd184eaa17a8f
AnnMm85 <8mona854@gmail.com>
2024-04-12 13:11:30 +0700

    Реализация страницы создания групп

--- pop-it-mvc/routes/web.php
+++ pop-it-mvc/routes/web.php
@@ -10,6 +10,7 @@ Route::add('GET', '/disciplines', [Controller\Site::class, 'disciplines'])->midd
 Route::add('GET', '/grades', [Controller\Site::class, 'grades'])->middleware('auth','sotrudnik');
 Route::add(['GET', 'POST'], '/discipline_groops', [Controller\Site::class, 'discipline_groops'])->middleware('auth','sotrudnik');
 Route::add(['GET', 'POST'], '/create_students', [Controller\Site::class, 'create_students'])->middleware('auth','sotrudnik');
+Route::add(['GET', 'POST'], '/create_groops', [Controller\Site::class, 'create_groops'])->middleware('auth','sotrudnik');
 
 
 
---
+++ pop-it-mvc/views/site/create_groops.php
@@ -0,0 +1,6 @@
+<a class="oneabutt" href="<?= app()->route->getUrl('/groops') ?>">Все группы</a>
+<h2>Добавление группы</h2>
+<form method="post" class="form">
+    <label>Номер группы <input type="text" name="name"></label>
+    <button class="butt-form">Добавить</button>
+</form>
