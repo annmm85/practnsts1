10a5f1022ce38063ffeff7e21e7e19d59cd5398f
AnnMm85 <8mona854@gmail.com>
2024-04-12 13:06:21 +0700

    Реализация страницы вывода дисциплин

--- pop-it-mvc/routes/web.php
+++ pop-it-mvc/routes/web.php
@@ -6,6 +6,7 @@ Route::add('GET', '/users', [Controller\Site::class, 'users'])->middleware('auth
 Route::add('GET', '/students', [Controller\Site::class, 'students'])->middleware('auth','sotrudnik');
 Route::add(['GET', 'POST'], '/create_disciplines', [Controller\Site::class, 'create_disciplines'])->middleware('auth','sotrudnik');
 Route::add('GET', '/groops', [Controller\Site::class, 'groops'])->middleware('auth','sotrudnik');
+Route::add('GET', '/disciplines', [Controller\Site::class, 'disciplines'])->middleware('auth','sotrudnik');
 
 
 
---
+++ pop-it-mvc/views/site/disciplines.php
@@ -0,0 +1,39 @@
+
+<div class="topbutt" ><a class="abutt" href="<?= app()->route->getUrl('/disciplines') ?>">Все дисциплины</a>
+    <a class="abutt" href="/pop-it-mvc/create_disciplines">Добавить дисциплину</a>
+</div>
+<h1 class="block-title">Дисциплины</h1>
+<div  class="filtermainblock">
+    <div class="filterblock">
+        <h3 class="block-title"> Курсы</h3>
+        <?php
+
+        echo '<ol  class="olioli">';
+        foreach ($findDisciplines as $dis) {
+            echo '<li>' . '<a  class="filterelem"  href="/pop-it-mvc/disciplines?course_id=' . $dis->course_id. '">' . $dis->course_id. '</a>' . '</li>';
+        }
+        echo '</ol>';
+        ?>
+    </div>
+
+<div class="filterblock">
+<h3 class="block-title">Семестры</h3>
+<ol class="olioli">
+    <li class="filterelem">1</li>
+    <li class="filterelem">2</li>
+</ol></div>
+    </div>
+
+<?php
+if ($findDisciplines) {
+    echo '<div class="findspis">';
+    foreach ($findDisciplines as $dis) {
+        echo '<div  class="findelemspis">';
+        echo '<li>' . $dis->name . '</li>';
+        echo '</div>';
+    }
+    echo '</div>';
+}
+?>
+
+
