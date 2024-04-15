dfeac24b4b13854578205ab0624219282725ad9f
AnnMm85 <8mona854@gmail.com>
2024-04-15 08:59:29 +0700

    Реализация валидаций

---
+++ pop-it-mvc/core/Src/Validator/DaterValidator.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace Src\Validator;
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+use Src\Validator\AbstractValidator;
+
+class DaterValidator extends AbstractValidator
+{
+
+    protected string $message = 'Поле :field может содержать только кириллицу';
+
+    public function rule(): bool
+    {
+        return (strtotime($this->value) < strtotime(date('Y-m-d')));
+    }
+}
---
+++ pop-it-mvc/core/Src/Validator/NumberValidator.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Src\Validator;
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+use Src\Validator\AbstractValidator;
+
+class NumberValidator extends AbstractValidator
+{
+
+    protected string $message = 'В :field должны быть буквы';
+
+    public function rule(): bool
+    {
+        return !ctype_digit($this->value);
+    }
+}
