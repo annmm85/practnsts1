ef303dbb9719249fedaa2cee36f6f39cde4e0ca7
AnnMm85 <8mona854@gmail.com>
2024-04-16 19:02:54 +0700

    lab4

--- vendor/annmm85/uplod/src/helpers.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Uplod;
-
-
-function uplodition(string $direct_name = '', array $files = [], string $uniqfile = ''): Uplod
-{
-    return new Uplod($direct_name, $files, $uniqfile);
-}
